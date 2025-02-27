a50d5b31c97b19790c6baea620c7afda5f4d47c3
NguyenThanhBinh1210 <thanhbinh12369yt@gmail.com>
2025-02-27 22:19:22 +0700

    binh

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -128,7 +128,7 @@ const Header = () => {
         <div className='flex items-center gap-2 justify-end'>
           <Link to={'/contact-now'} className='uppercase'>
             <button
-              className={`uppercase hover:opacity-65 transition-all duration-300 border  px-4 py-2 ${bgColor === 'linear-gradient(180deg,#ffffff,#ffffff)' ? 'border-black' : 'border-white text-white'
+              className={`uppercase hover:opacity-65 text-sm md:text-base transition-all duration-300 border  px-4 py-2 ${bgColor === 'linear-gradient(180deg,#ffffff,#ffffff)' ? 'border-black' : 'border-white text-white'
                 }`}
             >
               Đặt ngay
@@ -166,81 +166,7 @@ const Header = () => {
                 <path d='M2 12H22' />
               </svg>
             </button>
-            <button onClick={() => setIsNavOpen('shop')} className={` flex items-center gap-4 uppercase group `}>
-              Shop{' '}
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='lucide lucide-move-right transition-all duration-300 group-hover:translate-x-2'
-              >
-                <path d='M18 8L22 12L18 16' />
-                <path d='M2 12H22' />
-              </svg>
-            </button>
-            <button onClick={() => setIsNavOpen('dine')} className={` flex items-center gap-4 uppercase group `}>
-              Dine{' '}
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='lucide lucide-move-right transition-all duration-300 group-hover:translate-x-2'
-              >
-                <path d='M18 8L22 12L18 16' />
-                <path d='M2 12H22' />
-              </svg>
-            </button>
-            <button onClick={() => setIsNavOpen('seedo')} className={` flex items-center gap-4 uppercase group `}>
-              See & Do{' '}
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='lucide lucide-move-right transition-all duration-300 group-hover:translate-x-2'
-              >
-                <path d='M18 8L22 12L18 16' />
-                <path d='M2 12H22' />
-              </svg>
-            </button>
-            <Link onClick={toggleMenu} to={'/expo'} className={` flex items-center gap-4 uppercase group `}>
-              What's On
-            </Link>
-            <button onClick={() => setIsNavOpen('paiza')} className={` flex items-center gap-4 uppercase group `}>
-              Paiza{' '}
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='lucide lucide-move-right transition-all duration-300 group-hover:translate-x-2'
-              >
-                <path d='M18 8L22 12L18 16' />
-                <path d='M2 12H22' />
-              </svg>
-            </button>
+
             <button onClick={() => setIsNavOpen('Rewards')} className={` flex items-center gap-4 uppercase group `}>
               Rewards
               <svg
